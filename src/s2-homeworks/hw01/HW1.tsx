cd20cf1d35665f484301be24df819e003efd7d6b
ergentus <ponfable999@gmail.com>
2023-03-03 14:24:31 +0300

    task-01

--- src/s2-homeworks/hw01/HW1.tsx
+++ src/s2-homeworks/hw01/HW1.tsx
@@ -13,36 +13,49 @@ import avatar from './avatar.png'
 * 5 - сделать стили в соответствии с дизайном
 * */
 
-// нужно создать правильный тип вместо any
-export type MessageType = any
+type UserType = {
+    avatar: string
+    name: string
+}
 
-// структуру объекта не менять
+type innerType = {
+    text: string
+    time: string
+}
+
+export type MessageType = {
+    id: number
+    user: UserType
+    message: innerType
+}
+
+
 export const message0: MessageType = {
     id: 0,
     user: {
-        avatar: avatar, // можно менять
-        name: 'Some Name',  // можно менять
+        avatar: avatar, 
+        name: 'Some Name',  
     },
     message: {
-        text: 'some textsome textsome textsome textsome textsome textsome text', // можно менять
-        time: '22:00', // можно менять
+        text: 'Howdy!', 
+        time: '22:00', 
     },
 }
 export const friendMessage0: MessageType = {
     id: 100,
     user: {
-        avatar: avatar, // можно менять
-        name: 'Friend Name', // можно менять
+        avatar: avatar, 
+        name: 'Friend Name', 
     },
     message: {
-        text: 'зеркальное сообщение для тренировки css', // можно менять
-        time: '22:00', // можно менять
+        text: 'Hi! Hru, what did u do yesterday?', 
+        time: '22:00', 
     },
 }
 
 const HW1 = () => {
     return (
-        <div id={'hw1'}>
+        <div id={'hw1'} className='container'>
             <div className={s2.hwTitle}>Homework #1</div>
             <div className={s2.hw}>
                 {/*проверка отображения (не менять)*/}
@@ -50,8 +63,6 @@ const HW1 = () => {
                     <Message message={message0} />
                     <FriendMessage message={friendMessage0} />
                 </div>
-
-                {/*для автоматической проверки дз (не менять)*/}
                 <MessageSender M={Message} />
             </div>
         </div>
--- src/s2-homeworks/hw01/message/Message.tsx
+++ src/s2-homeworks/hw01/message/Message.tsx
@@ -1,38 +1,24 @@
 import React from 'react'
+import {MessageType} from '../HW1'
 import s from './Message.module.css'
 
-// нужно создать правильный тип вместо any
-export type MessagePropsType = any
 
-// нужно отобразить приходящие данные
+type MessagePropsType = {
+    message: MessageType
+}
+
+
 const Message = (props: MessagePropsType) => {
     return (
         <div id={'hw1-message-' + props.message.id} className={s.message}>
             <div className={s.imageAndText}>
-                <img
-                    id={'hw1-avatar-' + props.message.id}
-                    // создаёт студент
-
-                    //
-                />
+                <img id={'hw1-avatar-' + props.message.id} alt='avatar' src={props.message.user.avatar}/>
                 <div className={s.text}>
-                    <div id={'hw1-name-' + props.message.id} className={s.name}>
-                        {/*создаёт студент*/}
-
-                        {/**/}
-                    </div>
-                    <pre id={'hw1-text-' + props.message.id} className={s.messageText}>
-                        {/*создаёт студент*/}
-
-                        {/**/}
-                    </pre>
+                    <div id={'hw1-name-' + props.message.id} className={s.name}>{props.message.user.name}</div>
+                    <pre id={'hw1-text-' + props.message.id} className={s.messageText}>{props.message.message.text}</pre>
                 </div>
             </div>
-            <div id={'hw1-time-' + props.message.id} className={s.time}>
-                {/*создаёт студент*/}
-
-                {/**/}
-            </div>
+            <div id={'hw1-time-' + props.message.id} className={s.time}>{props.message.message.time}</div>
         </div>
     )
 }
