a4095302644c85fc5089c20bf0cb71b5052a8451
ergentus <ponfable999@gmail.com>
2023-04-07 06:12:24 +0300

    solution-06

--- src/s2-homeworks/hw06/localStorage/localStorage.ts
+++ src/s2-homeworks/hw06/localStorage/localStorage.ts
@@ -1,16 +1,14 @@
 // вот вам функция для сохранения объектов в память браузера
 // (данные в этом хранилище сохраняться даже при перезагрузке компа):
 export function saveState<T>(key: string, state: T) {
-    const stateAsString = JSON.stringify(state)
-    localStorage.setItem(key, stateAsString)
+    localStorage.setItem(key, JSON.stringify(state))
 }
 
 // и вот вам функция для получения сохранённого объекта в памяти браузера:
 export function restoreState<T>(key: string, defaultState: T) {
-    let state = defaultState
     const stateAsString = localStorage.getItem(key)
-    if (stateAsString !== null) state = JSON.parse(stateAsString) as T
-    return state
+    if (stateAsString) defaultState = JSON.parse(stateAsString) as T
+    return defaultState
 }
 
 // ---------------------------------------------------------------------------------------------------------------
