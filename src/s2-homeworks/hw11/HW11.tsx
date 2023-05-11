63e11b47b10b7a494154e3d3e4c905b21ced13ac
ergentus <ponfable999@gmail.com>
2023-05-12 01:43:33 +0300

    solution11 + css

--- src/s2-homeworks/hw11/HW11.tsx
+++ src/s2-homeworks/hw11/HW11.tsx
@@ -1,7 +1,7 @@
-import React, { useState } from 'react'
+import React, {useState} from 'react'
 import s from './HW11.module.css'
 import s2 from '../../s1-main/App.module.css'
-import { restoreState } from '../hw06/localStorage/localStorage'
+import {restoreState} from '../hw06/localStorage/localStorage'
 import SuperRange from './common/c7-SuperRange/SuperRange'
 
 /*
@@ -11,41 +11,56 @@ import SuperRange from './common/c7-SuperRange/SuperRange'
 * */
 
 function HW11() {
-    // for autotests // не менять // можно подсунуть в локалСторэдж нужные числа, чтоб увидеть как они отображаются
-    const [value1, setValue1] = useState(restoreState<number>('hw11-value1', 0))
-    const [value2, setValue2] = useState(restoreState<number>('hw11-value2', 100))
+	// for autotests // не менять // можно подсунуть в локалСторэдж нужные числа, чтоб увидеть как они отображаются
+	let [value1, setValue1] = useState(restoreState<number>('hw11-value1', 0))
+	let [value2, setValue2] = useState(restoreState<number>('hw11-value2', 100))
 
-    const change = (event: any, value: any) => {
-        // пишет студент // если пришёл массив - сохранить значения в оба useState, иначе в первый
-    }
+	const change = (event: any, value: number | number[]) => {
+		// пишет студент // если пришёл массив - сохранить значения в оба useState, иначе в первый
 
-    return (
-        <div id={'hw11'}>
-            <div className={s2.hwTitle}>Homework #11</div>
+		if (Array.isArray(value)) {
+			setValue1(value[0])
+			setValue2(value[1])
+		} else {
+			setValue1(value)
+		}
+	}
 
-            <div className={s2.hw}>
-                <div className={s.container}>
-                    <div className={s.wrapper}>
-                        <span id={'hw11-value'} className={s.number}>{value1}</span>
-                        <SuperRange
-                            id={'hw11-single-slider'}
-                            // сделать так чтоб value1 изменялось // пишет студент
 
-                        />
-                    </div>
-                    <div className={s.wrapper}>
-                        <span id={'hw11-value-1'} className={s.number}>{value1}</span>
-                        <SuperRange
-                            id={'hw11-double-slider'}
-                            // сделать так чтоб value1/2 изменялось // пишет студент
+	return (
+		<div id={'hw11'}>
+			<div className={s2.hwTitle}>Homework #11</div>
 
-                        />
-                        <span id={'hw11-value-2'} className={s.number}>{value2}</span>
-                    </div>
-                </div>
-            </div>
-        </div>
-    )
+			<div className={s2.hw}>
+				<div className={s2.main}>
+					<div className={s.container}>
+						<div className={s.wrapper}>
+							<div id={'hw11-value'} className={s.number}>{value1}</div>
+							<SuperRange
+								id={'hw11-single-slider'}
+								onChange={change}
+								min={0}
+								max={100}
+								value={value1}
+
+							/>
+						</div>
+						<div className={s.wrapper}>
+							<div id={'hw11-value-1'} className={s.number}>{value1}</div>
+							<SuperRange
+								id={'hw11-double-slider'}
+								onChange={change}
+								min={0}
+								max={100}
+								value={[value1, value2]}
+							/>
+							<div id={'hw11-value-2'} className={s.number}>{value2}</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	)
 }
 
 export default HW11
--- src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
+++ src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
@@ -4,10 +4,42 @@ import {Slider, SliderProps} from '@mui/material'
 const SuperRange: React.FC<SliderProps> = (props) => {
     return (
         <Slider
-            sx={{ // стили для слайдера // пишет студент
-                
+            sx={{
+               '& .MuiSlider-thumb': {
+                  backgroundColor: 'white',
+                  border: '2px solid #00CC22',
+                  width: '20px',
+                  height: '20px',
+                  boxShadow: '0 0 0 0 rgba(0, 204, 34, 0.30)',
+                  '&:hover': {
+                     boxShadow: '0 0 0 8px rgba(0, 204, 34, 0.30)',
+                  },
+                  '&:active': {
+                     boxShadow: '0 0 0 11px rgba(0, 204, 34, 0.30)',
+                  },
+               },
+               '& .MuiSlider-thumb::before': {
+                  content: '""',
+                  backgroundColor: '#00CC22',
+                  width: '6px',
+                  height: '6px',
+                  borderRadius: '50%',
+                  position: 'absolute',
+                  top: '50%',
+                  left: '50%',
+                  transform: 'translate(-50%, -50%)',
+                  boxShadow: '0 0 0 0 rgba(0, 204, 34, 0.30)',
+               },
+               '& .MuiSlider-track': {
+                  backgroundColor: '#00CC22',
+                  border: '3px solid #00CC22',
+               },
+               '& .MuiSlider-rail': {
+                  backgroundColor: '#8B8B8B',
+                  border: '3px solid #8B8B8B',
+               },
             }}
-            {...props} // отдаём слайдеру пропсы если они есть (value например там внутри)
+            {...props}
         />
     )
 }
